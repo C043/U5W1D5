2308d9058556fd2e9136ff55a1ba600e45bfdd18
C043 <mariofragnitoph@gmail.com>
2024-09-06 15:13:01 +0200

    Update PrenotazioneService.java

--- src/main/java/fragnito/U5W1D5/services/PrenotazioneService.java
+++ src/main/java/fragnito/U5W1D5/services/PrenotazioneService.java
@@ -31,8 +31,4 @@ public class PrenotazioneService {
         prenotazioneRepository.delete(this.findPrenotazioneById(prenotazioneId));
         log.info("Prenotazione con id: {} è stata eliminata con successo!", prenotazioneId);
     }
-
-    public void findPrenotazioneByIdAndUpdate(Long prenotazioneId, Prenotazione updatedPrenotazione) {
-        Prenotazione found = this.findPrenotazioneById(prenotazioneId);
-    }
 }
