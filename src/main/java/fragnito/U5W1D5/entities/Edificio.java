03b12076957aad704c69367b943f11941268f604
C043 <mariofragnitoph@gmail.com>
2024-09-06 10:35:36 +0200

    Adds Edificio

---
+++ src/main/java/fragnito/U5W1D5/entities/Edificio.java
@@ -0,0 +1,32 @@
+package fragnito.U5W1D5.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "edifici")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Edificio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String indirizzo;
+
+    @Column(name = "città", nullable = false)
+    private String citta;
+
+    public Edificio(String nome, String indirizzo, String citta) {
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.citta = citta;
+    }
+}
